590188a0bedbf8c39bd92a4fdaf2de97521164d3
CHEN-YI HUANG <chenyee@cs.nctu.edu.tw>
2019-10-13 15:59:37 +0800

    :penguin: FInish pybind11

--- hw2/titaneric/Q2/angle.cpp
+++ hw2/titaneric/Q2/angle.cpp
@@ -1,13 +1,15 @@
-#include <cmath>
-#include <iostream>
-#include "../Q1/line.h"
+#include <pybind11/pybind11.h>
 
-using namespace std;
-float dot(Line vec1, Line vec2)
+#include <cmath>
+#include <utility>
+
+namespace py = pybind11;
+typedef std::pair<float, float> Vector;
+float dot(Vector vec1, Vector vec2)
 {
-    return vec1.x(0) * vec2.x(0) + vec1.y(0) * vec2.y(0);
+    return vec1.first * vec2.first + vec1.second * vec2.second;
 }
-float getAngle(Line vec1, Line vec2)
+float getAngle(Vector vec1, Vector vec2)
 {
     float dot_result = dot(vec1, vec2);
     float vec1_len = sqrt(dot(vec1, vec1));
@@ -15,15 +17,7 @@ float getAngle(Line vec1, Line vec2)
     return acos(dot_result / (vec1_len * vec2_len));
 }
 
-int main()
-{
-    Line vec1(1);
-    vec1.x(0) = 0;
-    vec1.y(0) = 1;
-
-    Line vec2(1);
-    vec2.x(0) = 1;
-    vec2.y(0) = 0;
-    
-    cout << getAngle(vec1, vec2) << endl;
+PYBIND11_MODULE(angle, m) {
+    m.doc() = "pybind11 angle plugin"; // optional module docstring
+    m.def("getAngle", &getAngle, "A function which calculates angle between two vectors");
 }
