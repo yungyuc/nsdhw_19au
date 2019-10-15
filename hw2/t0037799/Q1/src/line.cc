0c34e0fe38af27cc8cb877f45031186d20a183f0
t0037799 <asdqerdfethtte@gmail.com>
2019-10-15 18:55:59 +0800

    line class refine

--- hw2/t0037799/Q1/src/line.cc
+++ hw2/t0037799/Q1/src/line.cc
@@ -1,13 +1,13 @@
 #include "line.h"
 
 Line::Line(Line const &line) { this->datas_ = line.datas_; }
-Line::Line(Line &&line) { datas_.swap(line.datas_); }
+Line::Line(Line &&line) { datas_ = std::move(line.datas_); }
 Line &Line::operator=(Line const &line) {
   this->datas_ = line.datas_;
   return *this;
 }
 Line &Line::operator=(Line &&line) {
-  datas_.swap(line.datas_);
+  this->datas_ = std::move(line.datas_);
   return *this;
 }
 Line::Line(size_t size) { datas_ = std::vector<std::pair<float, float>>(size); }
