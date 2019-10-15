c1cd261e60c4d89b60082fef93fa50d34e82d794
alan0201tw <alan0201tw@gmail.com>
2019-10-15 16:54:34 +0800

    add hw2, angle computation, unit-test

--- hw2/alan0201tw/Q2/pybind_test.py
+++ hw2/alan0201tw/Q2/pybind_test.py
@@ -1,7 +1,80 @@
 import numpy as np
 from vector import *
 
+# import nose for unit-test
+from nose import *
 
-print(rotate((1,0), np.pi/4))
-print(rotate((1,0), np.pi/2))
-print(rotate((1,0), np.pi))
+c_PI = np.pi
+epsilon = np.finfo(np.float64).eps
+
+################################################
+##
+##  Setting up
+##
+################################################
+
+def setup_module(module):
+    print ("") # this is to get a newline after the dots
+    print ("========================================================================")
+    print ("NOSE_SETUP : Setting up nosetest environment...")
+    print ("NOSE_SETUP : using constant PI from numpy, PI = ", c_PI)
+    print ("NOSE_SETUP : using constant epsilon for type 'double' from numpy, epsilon = ", epsilon)
+    print ("========================================================================")
+
+################################################
+##
+##  Helper functions
+##
+################################################
+
+def expect_difference_within_epsolon(a, b):
+    return ( np.abs(a - b) ) <= epsilon
+
+################################################
+##
+##  Unit tests
+##
+################################################
+
+def test_zero_to_zero():
+    assert np.isnan(compute_angle((0, 0), (0, 0)))
+
+def test_unitX_to_unitNegX():
+    assert expect_difference_within_epsolon(
+        compute_angle((1, 0), (-1, 0)),
+        c_PI)
+
+def test_unitX_to_unitX():
+    assert expect_difference_within_epsolon(
+        compute_angle((1, 0), (1, 0)),
+        0)
+
+def test_unitX_to_unitY():
+    assert expect_difference_within_epsolon(
+        compute_angle((1, 0), (0, 1)),
+        c_PI / 2)
+
+def test_unitX_to_unitXY():
+    assert expect_difference_within_epsolon(
+        compute_angle((1, 0), (1, 1)),
+        c_PI / 4)
+
+def test_unitXY_to_unitNegXY():
+    assert expect_difference_within_epsolon(
+        compute_angle((1, 1), (-1, -1)),
+        c_PI)
+
+def test_identical_vector():
+    assert expect_difference_within_epsolon(
+        compute_angle((0.78221, 1.12313231), (0.78221, 1.12313231)),
+        0)
+
+def test_identical_vector_to_neg():
+    assert expect_difference_within_epsolon(
+        compute_angle((0.78221, 1.12313231), (-0.78221, -1.12313231)),
+        c_PI)
+
+def test_random_test_case():
+    assert expect_difference_within_epsolon(
+        compute_angle((3, 4), (-4, 3)),
+        c_PI / 2)
--- hw2/alan0201tw/Q2/vector.cpp
+++ hw2/alan0201tw/Q2/vector.cpp
@@ -3,28 +3,44 @@
 #include <cmath>
 #include <tuple>
 
-std::tuple<double, double> rotate(
-    std::tuple<double, double> const & vec
-  , double rad
-)
+double compute_angle(
+    std::tuple<double, double> const & vec0,
+    std::tuple<double, double> const & vec1
+    )
 {
+    const double x0 = std::get<0>(vec0);
+    const double y0 = std::get<1>(vec0);
+    const double x1 = std::get<0>(vec1);
+    const double y1 = std::get<1>(vec1);
 
-    const double cth = cos(rad);
-    const double sth = sin(rad);
+    const double dot = x0*x1 + y0*y1;
+    const double det = x0*y1 - y0*x1;
 
-    return std::tuple<double, double>(
-        std::get<0>(vec) * cth - std::get<1>(vec) * sth
-      , std::get<0>(vec) * sth + std::get<1>(vec) * cth);
+    // Reference : http://www.cplusplus.com/reference/cmath/atan2/
+    //
+    // If both arguments passed are zero, a domain error occurs.
+    //
+    // If a domain error occurs:
+    // - And math_errhandling has MATH_ERRNO set: the global variable errno is set to EDOM.
+    // - And math_errhandling has MATH_ERREXCEPT set: FE_INVALID is raised.
+    //
+    // Since we only use atan2 from the C++ standard math library, instead
+    // of doing some proper error handling, I just return a nan value when
+    // the arguments are not proper for atan2, and leave it to the python
+    // side to deal with it.
 
+    if(det == 0.0 && dot == 0.0)
+        return nan("");
+
+    return std::atan2(det, dot);
 }
 
 PYBIND11_MODULE(vector, mod)
 {
-
     mod.doc() = "example C extension module";
-
-    mod.def("rotate", &rotate, "vector rotation");
-
+    mod.def("compute_angle", &compute_angle, 
+        "calculates the angle (in radians) between two vectors in the "
+        "2-dimensional Cartesian coordinate system");
 }
 
 /* vim: set et ts=4 sw=4: */
