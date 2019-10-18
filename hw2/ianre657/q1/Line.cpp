9e1353fb083dde93159ff3cf258c3145332f580d
Ian Chen <ianre657@gmail.com>
2019-10-19 01:02:23 +0800

    :ok_hand: q1 done

--- hw2/ianre657/q1/Line.cpp
+++ hw2/ianre657/q1/Line.cpp
@@ -1,57 +1,62 @@
 #include "Line.hpp"
+#include <stdexcept>
+#include <vector>
 
-size_t Line::get_x_shift(size_t it){
-
+size_t Line::check_range(size_t it) const {
+  if (it >= data_ptr->size()) {
+    throw std::out_of_range("Line index out of range");
+  }
+  return it;
 }
 
-size_t Line::get_y_shift(size_t it){
+size_t get_x_shift(size_t it) { return it << 1; }
 
+size_t get_y_shift(size_t it) { return (it << 1) + 1; }
+
+Line::Line() : m_size(0) { data_ptr = new std::vector<float>{}; }
+
+// copy constructor
+Line::Line(Line const &other) : m_size(other.m_size) {
+  data_ptr =
+      new std::vector<float>{other.data_ptr->begin(), other.data_ptr->end()};
 }
 
-Line::Line() = default;
+// move constructor
+Line::Line(Line &&other) : m_size(other.m_size) { data_ptr = other.data_ptr; }
 
-//copy constructor
-Line::Line(Line const & other){
-
+Line &Line::operator=(Line const &other) {
+  data_ptr =
+      new std::vector<float>{other.data_ptr->begin(), other.data_ptr->end()};
+  m_size = other.m_size;
+  return *this;
 }
 
-//move constructor
-Line::Line(Line && other){
-
+Line &Line::operator=(Line &&other) {
+  data_ptr = other.data_ptr;
+  m_size = other.m_size;
+  return *this;
 }
 
-Line & Line::operator=(Line const & other){
-
+Line::Line(size_t size) {
+  data_ptr = new std::vector<float>{};
+  data_ptr->resize(2 * size);
+  m_size = size;
 }
 
-Line & Line::operator=(Line && other){
+Line::~Line() { delete data_ptr; }
 
+size_t Line::size() const { return m_size; }
+
+float &Line::x(size_t it) const {
+  size_t idx = get_x_shift(it);
+  check_range(idx);
+  return (*data_ptr)[idx];
 }
 
-Line::Line(size_t size){
+float &Line::x(size_t it) { return (*data_ptr)[check_range(get_x_shift(it))]; }
 
+float &Line::y(size_t it) const {
+  return (*data_ptr)[check_range(get_y_shift(it))];
 }
 
-Line::~Line(){
-
-}
-
-size_t Line::size() const{
-
-}
-
-float & Line::x(size_t it) const{
-
-}
-
-float & Line::x(size_t it){
-
-}
-
-float & Line::y(size_t it) const{
-
-}
-
-float &Line::y(size_t it){
-
-}
+float &Line::y(size_t it) { return (*data_ptr)[check_range(get_y_shift(it))]; }
