8db2820e61d970e87497b6d17d02db3f1eb3f100
Evan tsai <evan11401@gmail.com>
2019-10-20 21:21:37 +0800

    finish q2

--- hw2/evan11401/q2/angle.cpp
+++ hw2/evan11401/q2/angle.cpp
@@ -1,14 +1,9 @@
 #include<pybind11/pybind11.h>
 #include<cmath>
 using namespace std;
-double dot(pair<double,double> vec1, pair<double,double> vec2)
-{
-    return vec1.first * vec2.first + vec1.second * vec2.second;
-}
 double calculateAngle(pair<double,double> vec1,pair<double,double> vec2){
 	double rawAngle = atan2(vec2.second, vec2.first) - atan2(vec1.second, vec1.first);
-	if(rawAngle<0) rawAngle+= 2 * M_PI;
-	return rawAngle;
+	return (rawAngle<0)?rawAngle+2*M_PI:rawAngle;
 }
 PYBIND11_MODULE(angle, m) {
     m.doc() = "pybind11 calculate Angle from two vectors";
