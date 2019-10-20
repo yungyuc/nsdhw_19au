1fba2b6fff22d1a363765c5cc7963293c03bec99
chi_lin lu <lujames13@gmail.com>
2019-10-20 11:26:05 -0700

    vec2angle func complete

--- hw2/lujames13/q2/vec2angle.cpp
+++ hw2/lujames13/q2/vec2angle.cpp
@@ -1,18 +1,16 @@
 #include <pybind11/pybind11.h>
-
+#include <cmath>
 namespace py = pybind11;
 
-// simple function to test if python can import package or not
-int add(int i, int j){
-	return i + j;
+double vec2angle(std::pair<double, double> v1, std::pair<double, double> v2){
+	double nomalization = sqrt(pow(v1.first, 2) + pow(v1.second, 2)) * sqrt(pow(v2.first, 2) + pow(v2.second, 2));
+	double dot = v1.first * v2.first + v1.second * v2.second;
+	double angle = acos(dot/nomalization);
+	return angle;
 }
 
-
 PYBIND11_MODULE(vec2angle, m){
-	m.doc() = "pybin11 example plugin";
-	m.def("add", &add, "A function which adds two numbers", py::arg("i")=1, py::arg("j")=2);
+	m.doc() = "pybin11 plugin to calculate the angle between 2 vectors";
+	m.def("vec2angle", &vec2angle, "A function which calculate the angle between 2 vectors", py::arg("v1"), py::arg("v2"));
 
-	m.attr("the_answer") = 42;
-	py::object world = py::cast("World");
-	m.attr("what") = world;
 }
