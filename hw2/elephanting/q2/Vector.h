45dbd5e5583dea4ef746d9c0ecb8d67305d8ad2b
elephanting <elephanting5566@gmail.com>
2019-10-22 01:20:46 +0800

    q2 done

--- hw2/elephanting/q2/Vector.h
+++ hw2/elephanting/q2/Vector.h
@@ -3,8 +3,8 @@ class Vector
 public:
     Vector(float, float);
     float len();
-    float getX();
-    float getY();
+    float getX() const;
+    float getY() const;
 private:
     float m_x;
     float m_y;
