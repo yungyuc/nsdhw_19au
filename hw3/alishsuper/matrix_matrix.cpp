21dce55935b98146db7c9d9e39414ce1a0dd9930
alishsuper <ali_2475@mail.ru>
2019-11-12 02:04:00 +0800

    finished hw3

---
+++ hw3/alishsuper/matrix_matrix.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <stdexcept>
+#include <mkl.h>
+#include <pybind11/stl.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+
+namespace py = pybind11;
+
+class Matrix {
+
+public:
+
+    Matrix(size_t nrow, size_t ncol)
+      : m_nrow(nrow), m_ncol(ncol)
+    {
+        reset_buffer(nrow, ncol);
+    }
+
+    Matrix(size_t nrow, size_t ncol, std::vector<double> const & vec)
+      : m_nrow(nrow), m_ncol(ncol)
+    {
+        reset_buffer(nrow, ncol);
+        (*this) = vec;
+    }
+
+    Matrix & operator=(std::vector<double> const & vec)
+    {
+        if (size() != vec.size())
+        {
+            throw std::out_of_range("number of elements mismatch");
+        }
+
+        size_t k = 0;
+        for (size_t i=0; i<m_nrow; ++i)
+        {
+            for (size_t j=0; j<m_ncol; ++j)
+            {
+                (*this)(i,j) = vec[k];
+                ++k;
+            }
+        }
+
+        return *this;
+    }
+
+    Matrix(Matrix const & other)
+      : m_nrow(other.m_nrow), m_ncol(other.m_ncol)
+    {
+        reset_buffer(other.m_nrow, other.m_ncol);
+        for (size_t i=0; i<m_nrow; ++i)
+        {
+            for (size_t j=0; j<m_ncol; ++j)
+            {
+                (*this)(i,j) = other(i,j);
+            }
+        }
+    }
+
+    Matrix & operator=(Matrix const & other)
+    {
+        if (this == &other) { return *this; }
+        if (m_nrow != other.m_nrow || m_ncol != other.m_ncol)
+        {
+            reset_buffer(other.m_nrow, other.m_ncol);
+        }
+        for (size_t i=0; i<m_nrow; ++i)
+        {
+            for (size_t j=0; j<m_ncol; ++j)
+            {
+                (*this)(i,j) = other(i,j);
+            }
+        }
+        return *this;
+    }
+
+    Matrix(Matrix && other)
+      : m_nrow(other.m_nrow), m_ncol(other.m_ncol)
+    {
+        reset_buffer(0, 0);
+        std::swap(m_nrow, other.m_nrow);
+        std::swap(m_ncol, other.m_ncol);
+        std::swap(m_buffer, other.m_buffer);
+    }
+
+    Matrix & operator=(Matrix && other)
+    {
+        if (this == &other) { return *this; }
+        reset_buffer(0, 0);
+        std::swap(m_nrow, other.m_nrow);
+        std::swap(m_ncol, other.m_ncol);
+        std::swap(m_buffer, other.m_buffer);
+        return *this;
+    }
+
+  bool operator==(Matrix &rhs) {
+    if (m_nrow != rhs.nrow() && m_ncol != rhs.ncol()) {
+      return false;
+    }
+    const double *rhs_buf = rhs.data();
+    size_t len = m_nrow * m_ncol;
+    for (size_t i = 0; i < len; ++i) {
+      if (m_buffer[i] != rhs_buf[i])
+        return false;
+    }
+    return true;
+  }
+
+    ~Matrix()
+    {
+        reset_buffer(0, 0);
+    }
+
+    double   operator() (size_t row, size_t col) const { return m_buffer[index(row, col)]; }
+    double & operator() (size_t row, size_t col)       { return m_buffer[index(row, col)]; }
+
+    size_t nrow() const { return m_nrow; }
+    size_t ncol() const { return m_ncol; }
+
+    size_t size() const { return m_nrow * m_ncol; }
+    double buffer(size_t i) const { return m_buffer[i]; }
+    std::vector<double> buffer_vector() const { return std::vector<double>(m_buffer, m_buffer+size()); }
+	double *data() { return m_buffer; }
+private:
+
+  size_t index(size_t row, size_t col) const
+  {
+  	return row * m_ncol + col;
+}
+
+
+    void reset_buffer(size_t nrow, size_t ncol)
+    {
+        if (m_buffer) { delete[] m_buffer; }
+        const size_t nelement = nrow * ncol;
+        if (nelement) { m_buffer = new double[nelement]; }
+        else          { m_buffer = nullptr; }
+        m_nrow = nrow;
+        m_ncol = ncol;
+    }
+
+    size_t m_nrow = 0;
+    size_t m_ncol = 0;
+    double * m_buffer = nullptr;
+
+};
+
+Matrix naive_multiplication(const Matrix &mat1, const Matrix &mat2) {
+    if (mat1.ncol() != mat2.nrow())
+    {
+        throw std::out_of_range(
+            "the number of first matrix column "
+            "differs from that of second matrix row");
+    }
+
+    Matrix ret(mat1.nrow(), mat2.ncol());
+
+    for (size_t i=0; i<ret.nrow(); ++i)
+    {
+        for (size_t k=0; k<ret.ncol(); ++k)
+        {
+            double v = 0;
+            for (size_t j=0; j<mat1.ncol(); ++j)
+            {
+                v += mat1(i,j) * mat2(j,k);
+            }
+            ret(i,k) = v;
+        }
+    }
+
+    return ret;
+}
+
+std::ostream & operator << (std::ostream & ostr, Matrix const & mat)
+{
+    for (size_t i=0; i<mat.nrow(); ++i)
+    {
+        ostr << std::endl << " ";
+        for (size_t j=0; j<mat.ncol(); ++j)
+        {
+            ostr << " " << std::setw(2) << mat(i, j);
+        }
+    }
+
+    return ostr;
+}
+Matrix mkl_multiplication(Matrix &mat1, Matrix &mat2) {
+  if (mat1.ncol() != mat2.nrow()) 
+  {
+    throw std::out_of_range("the number of first matrix column "
+                            "differs from that of second matrix row");
+  }
+    Matrix ret(mat1.nrow(), mat2.ncol());
+    cblas_dgemm(
+        CblasRowMajor,
+        CblasNoTrans,
+        CblasNoTrans,
+        mat1.nrow(),
+        mat2.ncol(),
+        mat1.ncol(),
+        1.0,
+        mat1.data(),
+        mat1.ncol(),
+        mat2.data(),
+        mat2.ncol(),
+        0.0,
+        ret.data(),
+        ret.ncol()
+    );
+
+  return ret;
+}
+
+PYBIND11_MODULE(_matrix, m) {
+  m.doc() = "pybind11 matrix plugin";
+  m.def("multiply_naive", &naive_multiplication, "Naive multiply of two matrix");
+  m.def("multiply_mkl", &mkl_multiplication, "MKL's cblas multiply of two matrix");
+  py::class_<Matrix>(m, "Matrix")
+      .def(py::init<size_t, size_t>())
+      .def_property_readonly("nrow", &Matrix::nrow)
+      .def_property_readonly("ncol", &Matrix::ncol)
+      .def("__eq__", [](Matrix &a, Matrix &b) { return a == b; })
+      .def("__getitem__",
+           [](Matrix &m, std::pair<size_t, size_t> i) {
+             return m(i.first, i.second);
+           })
+      .def("__setitem__", [](Matrix &m, std::pair<size_t, size_t> i,
+                             double v) { m(i.first, i.second) = v; });
+}
