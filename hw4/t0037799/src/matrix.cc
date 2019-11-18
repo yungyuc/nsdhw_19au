a5e2f209bba0b4c8cdc6754c325e6a60b071e832
t0037799 <asdqerdfethtte@gmail.com>
2019-11-18 15:19:10 +0800

    add tiling and change setter, getter

---
+++ hw4/t0037799/src/matrix.cc
@@ -0,0 +1,72 @@
+#include "matrix.h"
+#include <iomanip>
+#include <iostream>
+#include <mkl.h>
+#include <stdexcept>
+
+Matrix multiply_naive(Matrix &lhs, Matrix &rhs) {
+  if (lhs.ncol() != rhs.nrow()) {
+    throw std::invalid_argument("dimension mismatch");
+  }
+  Matrix ret(lhs.nrow(), rhs.ncol());
+  for (size_t i = 0; i < lhs.nrow(); ++i) {
+    for (size_t j = 0; j < rhs.ncol(); ++j) {
+      double v = 0;
+      for (size_t k = 0; k < lhs.ncol(); ++k) {
+        v += lhs(i, k) * rhs(k, j);
+      }
+      ret(i, j) = v;
+    }
+  }
+  return ret;
+}
+
+Matrix multiply_mkl(Matrix &lhs, Matrix &rhs) {
+  if (lhs.ncol() != rhs.nrow()) {
+    throw std::invalid_argument("dimension mismatch");
+  }
+  Matrix ret(lhs.nrow(), rhs.ncol());
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, lhs.nrow(), rhs.ncol(),
+              lhs.ncol(), 1, lhs.data(), lhs.ncol(), rhs.data(), rhs.ncol(), 0,
+              ret.data(), ret.ncol());
+  return ret;
+}
+
+Matrix multiply_tile(Matrix &lhs, Matrix &rhs, size_t tsize) {
+  if (lhs.ncol() != rhs.nrow()) {
+    throw std::invalid_argument("dimension mismatch");
+  }
+  Matrix ret(lhs.nrow(), rhs.ncol());
+  size_t s_i = (lhs.nrow() + tsize - 1) / tsize;
+  size_t s_j = (rhs.ncol() + tsize - 1) / tsize;
+  size_t s_k = (rhs.ncol() + tsize - 1) / tsize;
+  for (size_t t_i = 0; t_i < s_i; ++t_i) {
+    for (size_t t_j = 0; t_j < s_j; ++t_j) {
+      for (size_t t_k = 0; t_k < s_k; ++t_k) {
+        size_t r_i = std::min((t_i + 1) * tsize, lhs.nrow());
+        size_t r_j = std::min((t_j + 1) * tsize, rhs.ncol());
+        size_t r_k = std::min((t_k + 1) * tsize, lhs.ncol());
+        for (size_t i = t_i * tsize; i < r_i; ++i) {
+          for (size_t j = t_j * tsize; j < r_j; ++j) {
+            double v = 0;
+            for (size_t k = t_k * tsize; k < r_k; ++k) {
+              v += lhs(i, k) * rhs(k, j);
+            }
+            ret(i, j) += v;
+          }
+        }
+      }
+    }
+  }
+  return ret;
+}
+
+std::ostream &operator<<(std::ostream &os, Matrix const &m) {
+  for (size_t i = 0; i < m.nrow(); ++i) {
+    os << '\n';
+    for (size_t j = 0; j < m.ncol(); ++j) {
+      os << ' ' << std::setw(3) << m(i, j);
+    }
+  }
+  return os;
+}
---
+++ hw4/t0037799/src/module.cc
@@ -0,0 +1,32 @@
+#include "matrix.h"
+#include <pybind11/pybind11.h>
+#include <sstream>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_matrix, m) {
+  m.doc() = "simple matrix implementation";
+  m.def("multiply_naive", &multiply_naive);
+  m.def("multiply_mkl", &multiply_mkl);
+  m.def("multiply_tile", &multiply_tile);
+  py::class_<Matrix>(m, "Matrix")
+      .def(py::init<size_t, size_t>())
+      .def_property("nrow", &Matrix::nrow, nullptr)
+      .def_property("ncol", &Matrix::ncol, nullptr)
+      .def("__repr__",
+           [](const Matrix &m) {
+             std::stringstream ss;
+             ss << m;
+             return "<_matrix.Matrix size '" + std::to_string(m.nrow()) + 'x' +
+                    std::to_string(m.ncol()) + "'>\n" + ss.str();
+           })
+      .def("__eq__", [](const Matrix &m, const Matrix &n) { return m == n; })
+      .def("__getitem__",
+           [](const Matrix &m, std::pair<size_t, size_t> idx) {
+             return m.get(idx.first, idx.second);
+           })
+      .def("__setitem__",
+           [](Matrix &m, std::pair<size_t, size_t> idx, double v) {
+             return m.set(idx.first, idx.second) = v;
+           });
+}
