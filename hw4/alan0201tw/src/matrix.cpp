64749dfd1fbbdc9d19304c7e35af6314f0be91c9
DESKTOP-6J14KSJ\AlanShih <alan0201tw@gmail.com>
2019-11-19 22:25:43 +0800

    Remove redundant code

--- hw4/alan0201tw/src/matrix.cpp
+++ hw4/alan0201tw/src/matrix.cpp
@@ -103,90 +103,7 @@ std::ostream & operator << (std::ostream & ostr, Matrix const & mat)
     return ostr;
 }
 
-///////////////////////////////////////////
-//      Blocks and Tiles for Tiling      //
-///////////////////////////////////////////
-
-template<size_t N> void Block<N>::save(
-    Matrix & mat, size_t it, size_t jt
-)
-{
-    const size_t ncol = mat.ncol();
-
-    for (size_t i=0; i<NDIM; ++i)
-    {
-        const size_t base_s = i*NDIM;
-        const size_t base_t = (it*NDIM + i) * ncol + jt*NDIM;
-
-        for (size_t j=0; j<NDIM; ++j)
-        {
-            mat.m_buffer[base_t + j] = m_buffer[base_s + j];
-        }
-    }
-}
-
-template<size_t N> void Tiler<N>::load(
-    Matrix const & mat1, size_t it1, size_t jt1
-  , Matrix const & mat2, size_t it2, size_t jt2
-)
-{
-    const size_t ncol1 = mat1.ncol();
-
-    for (size_t i=0; i<NDIM; ++i)
-    {
-        const size_t base_t = i*NDIM;
-        const size_t base_s = (it1*NDIM + i) * ncol1 + jt1*NDIM;
-
-        for (size_t j=0; j<NDIM; ++j)
-        {
-            m_mat1[base_t + j] = mat1.m_buffer[base_s + j];
-        }
-    }
-
-    const size_t ncol2 = mat2.ncol();
-
-    for (size_t i=0; i<NDIM; ++i)
-    {
-        const size_t base_t = i*NDIM;
-        const size_t base_s = (it2*NDIM + i) * ncol2 + jt2*NDIM;
-
-        for (size_t j=0; j<NDIM; ++j)
-        {
-            m_ret[base_t + j] = mat2.m_buffer[base_s + j];
-        }
-    }
-
-    for (size_t i=0; i<NDIM; ++i)
-    {
-        const size_t base = i*NDIM;
-
-        for (size_t j=0; j<NDIM; ++j)
-        {
-            m_mat2[j*NDIM + i] = m_ret[base + j];
-        }
-    }
-}
-
-template<size_t N> void Tiler<N>::multiply()
-{
-    for (size_t i=0; i<NDIM; ++i)
-    {
-        const size_t base1 = i*NDIM;
-
-        for (size_t k=0; k<NDIM; ++k)
-        {
-            const size_t base2 = k*NDIM;
-
-            double v = 0;
-            for (size_t j=0; j<NDIM; ++j)
-            {
-                v += m_mat1[base1 + j] * m_mat2[base2 + j];
-            }
-            m_ret[base1 + k] = v;
-        }
-    }
-}
-
+//
 // Reference :
 // https://stackoverflow.com/questions/15829223/loop-tiling-blocking-for-large-dense-matrix-multiplication
 //
@@ -199,36 +116,39 @@ Matrix multiply_tile(Matrix const & mat1, Matrix const & mat2, const unsigned in
             "differs from that of second matrix row");
     }
 
-    // return multiply_mkl(mat1, mat2);
-
     Matrix ret(mat1.nrow(), mat2.ncol());
 
-    const size_t block_size = static_cast<int>(tsize);
+    const size_t tileSize = static_cast<size_t>(tsize);
     const size_t N = mat1.nrow();
     const size_t M = mat1.ncol();
     const size_t K = mat2.ncol();
 
-    for(size_t i=0; i<N; i++) {
-        for(size_t j=0; j<K; j++) {
+    for(size_t i=0; i<N; i++)
+    {
+        for(size_t j=0; j<K; j++)
+        {
             ret(i, j) = 0;
         }
     }
 
     // iterate by i,k,j instead of i,j,k gives another performance boost
+    // i k j for row major, i j k for column major
 
-    for (size_t i0 = 0; i0 < N; i0 += block_size) {
-        size_t imax = i0 + block_size > N ? N : i0 + block_size;
-
-        for (size_t k0 = 0; k0 < K; k0 += block_size) {
-                size_t kmax = k0 + block_size > K ? K : k0 + block_size;
-
-            for (size_t j0 = 0; j0 < M; j0 += block_size) {
-                size_t jmax = j0 + block_size > M ? M : j0 + block_size;
-
-                for (size_t i1 = i0; i1 < imax; ++i1) {
-                    for (size_t k1 = k0; k1 < kmax; ++k1) {
-                        for (size_t j1 = j0; j1 < jmax; ++j1) {
-                            // C[kij] += A[mi + k1] * B[sj + k1];
+    for (size_t i0 = 0; i0 < N; i0 += tileSize)
+    {
+        const size_t imax = i0 + tileSize > N ? N : i0 + tileSize;
+        for (size_t k0 = 0; k0 < K; k0 += tileSize)
+        {
+            const size_t kmax = k0 + tileSize > K ? K : k0 + tileSize;
+            for (size_t j0 = 0; j0 < M; j0 += tileSize)
+            {
+                const size_t jmax = j0 + tileSize > M ? M : j0 + tileSize;
+                for (size_t i1 = i0; i1 < imax; ++i1)
+                {
+                    for (size_t k1 = k0; k1 < kmax; ++k1)
+                    {
+                        for (size_t j1 = j0; j1 < jmax; ++j1)
+                        {
                             ret(i1, j1) += mat1(i1, k1) * mat2(k1, j1);
                         }
                     }
@@ -236,35 +156,7 @@ Matrix multiply_tile(Matrix const & mat1, Matrix const & mat2, const unsigned in
             }
         }
     }
-/*
-    const size_t nrow1 = mat1.nrow();
-    const size_t ncol1 = mat1.ncol();
-    // const size_t nrow2 = mat2.nrow();
-    const size_t ncol2 = mat2.ncol();
 
-    const size_t ntrow1 = nrow1 / tsize;
-    const size_t ntcol1 = ncol1 / tsize;
-    // const size_t ntrow2 = nrow2 / tsize;
-    const size_t ntcol2 = ncol2 / tsize;
-
-    Block<tsize> value;
-    Tiler<tsize> tiler;
-
-    for (size_t it=0; it<ntrow1; ++it)
-    {
-        for (size_t kt=0; kt<ntcol2; ++kt)
-        {
-            value = 0;
-            for (size_t jt=0; jt<ntcol1; ++jt)
-            {
-                tiler.load(mat1, it, jt, mat2, jt, kt);
-                tiler.multiply();
-                value += tiler.m_ret;
-            }
-            value.save(ret, it, kt);
-        }
-    }
-*/
     return ret;
 }
 
