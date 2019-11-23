64857f23724a969123fc80220df20b26a634639d
elephanting <elephanting5566@gmail.com>
2019-11-24 07:45:20 +0800

    hw4 initial commit

---
+++ hw4/elephanting/Matrix.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+class Matrix
+{
+public:
+    Matrix(size_t, size_t);
+    ~Matrix();
+    Matrix& operator=(Matrix const &);
+    bool operator==(Matrix const &);
+    Matrix(const Matrix &);
+    double& operator()(size_t const, size_t const);
+    double operator()(size_t const, size_t const) const;
+    double* buffer() const;
+    std::string repr() const;
+    size_t nrow() const;
+    size_t ncol() const;
+private:
+    size_t m_ncol;
+    size_t m_nrow;
+    double* m_buffer = nullptr;
+    void resetbuf(size_t, size_t);
+};
+
+Matrix mkl_MM(Matrix const &, Matrix const &);
+
+bool isequal(Matrix &, Matrix &);
+
+Matrix multiply_naive(Matrix const &, Matrix const &);
+
+Matrix operator*(Matrix const &, Matrix const &);
+
+/* broadcasting */
+Matrix operator*(double const &, Matrix const &);
+
+Matrix operator*(Matrix const &, double const &);
