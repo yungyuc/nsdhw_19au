5700491c6b0831e46c11c86db6348ccef24e2129
stveumi <stvreumi@livemail.tw>
2019-11-24 22:04:53 +0800

    organize tiling code

--- hw4/stvreumi/tiling.cpp
+++ hw4/stvreumi/tiling.cpp
@@ -1,31 +1,6 @@
 #include "tiling.h"
 
 
-template<size_t N>
-struct Block
-{
-    static constexpr const size_t NDIM = N;
-
-    double   operator[] (size_t idx) const { return m_buffer[idx]; }
-    double & operator[] (size_t idx)       { return m_buffer[idx]; }
-
-    Block<N> & operator= (double v)
-    {
-        for (size_t i=0; i<N*N; ++i) { m_buffer[i] = v; }
-        return *this;
-    }
-
-    Block<N> & operator+= (Block<N> const & other)
-    {
-        for (size_t i=0; i<N*N; ++i) { m_buffer[i] += other.m_buffer[i]; }
-        return *this;
-    }
-
-    void save(Matrix & mat, size_t it, size_t jt);
-
-    double m_buffer[N * N];
-};
-
 template<size_t N> void Block<N>::save(
     Matrix & mat, size_t it, size_t jt
 )
@@ -43,23 +18,6 @@ template<size_t N> void Block<N>::save(
         }
     }
 }
-
-template<size_t N>
-struct Tiler
-{
-    static constexpr const size_t NDIM = N;
-
-    void load(
-        Matrix const & mat1, size_t it1, size_t jt1
-      , Matrix const & mat2, size_t it2, size_t jt2
-    );
-
-    void multiply();
-
-    Block<N> m_mat1; // row-major
-    Block<N> m_mat2; // column-major
-    Block<N> m_ret; // row-major
-};
 
 template<size_t N> void Tiler<N>::load(
     Matrix const & mat1, size_t it1, size_t jt1
--- hw4/stvreumi/tiling.h
+++ hw4/stvreumi/tiling.h
@@ -4,32 +4,46 @@
 #define TILING_H
 
 template<size_t N>
-struct Block;
+struct Block
+{
+    static constexpr const size_t NDIM = N;
 
-template<size_t N> void Block<N>::save(
-    Matrix & mat, size_t it, size_t jt
-);
+    double   operator[] (size_t idx) const { return m_buffer[idx]; }
+    double & operator[] (size_t idx)       { return m_buffer[idx]; }
+
+    Block<N> & operator= (double v)
+    {
+        for (size_t i=0; i<N*N; ++i) { m_buffer[i] = v; }
+        return *this;
+    }
+
+    Block<N> & operator+= (Block<N> const & other)
+    {
+        for (size_t i=0; i<N*N; ++i) { m_buffer[i] += other.m_buffer[i]; }
+        return *this;
+    }
+
+    void save(Matrix & mat, size_t it, size_t jt);
+
+    double m_buffer[N * N];
+};
 
 template<size_t N>
-struct Tiler;
+struct Tiler
+{
+    static constexpr const size_t NDIM = N;
 
-template<size_t N> void Tiler<N>::load(
-    Matrix const & mat1, size_t it1, size_t jt1
-  , Matrix const & mat2, size_t it2, size_t jt2
-);
+    void load(
+        Matrix const & mat1, size_t it1, size_t jt1
+      , Matrix const & mat2, size_t it2, size_t jt2
+    );
 
-template<size_t N> void Tiler<N>::multiply();
+    void multiply();
 
-/*
- * Throw an exception if the shapes of the two matrices don't support
- * multiplication.
- */
-void validate_multiplication(Matrix const & mat1, Matrix const & mat2);
+    Block<N> m_mat1; // row-major
+    Block<N> m_mat2; // column-major
+    Block<N> m_ret; // row-major
+};
 
-/*
- * Tiled matrix matrix multiplication.
- */
-template<size_t LSIZE>
-Matrix multiply_tile(Matrix const & mat1, Matrix const & mat2);
 
 #endif //TILING_H
